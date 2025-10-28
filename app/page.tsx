693b000785809c3dfdde1b6246d064fa0d8386f1
Daksh Singh <63799338+evenrise01@users.noreply.github.com>
2025-10-28 19:51:26 +0530

    first commit

--- app/page.tsx
+++ app/page.tsx
@@ -2,64 +2,63 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
+    <>
+      <div className="container">
+        <div className="blurry-prev">
+          <img src="/img-1.jpg" alt="" />
+          <div className="overlay"></div>
         </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
+
+        <div className="col site-info">
+          <nav>
+            <a href="">Home</a>
+            <a href="">Work</a>
+            <a href="">Contact</a>
+          </nav>
+          <div className="header">
+            <h1>Daksh Singh</h1>
+          </div>
+
+          <div className="copy">
+            <p>
+              I am a freelance creative director delivering innovative design
+              solutions for businesses around the globe.
+            </p>
+          </div>
         </div>
-      </main>
-    </div>
+        <div className="col project-preview">
+          <div className="project-details">
+            <div className="title">
+              <h1>Beyond the Summit</h1>
+            </div>
+            <div className="info">
+              <p>
+                Join a team of elite mountaineers as they attempt to conquer K2
+                in winter, a feat never before accomplished. This breathtaking
+                documentary captures the raw beauty of the Karokam and the
+                indomitable human spirit.
+              </p>
+            </div>
+            <div className="credits">
+              <p>Credits</p>
+            </div>
+            <div className="director">
+              <p>Director: Alex Hornomi</p>
+            </div>
+            <div className="cinematographer">
+              <p>Cinematographer: Jimmy Chen</p>
+            </div>
+          </div>
+
+          <div className="project-img">
+            <img src="/img-1.jpg" alt="" />
+          </div>
+        </div>
+
+        <div className="gallery-wrapper">
+          <div className="gallery"></div>
+        </div>
+      </div>
+    </>
   );
 }
